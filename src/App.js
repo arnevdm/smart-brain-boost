15587e0cacda650c5fda19a0d1499adcdfcbd9ad
Vanimus <59360930+Vanimus@users.noreply.github.com>
2022-01-03 16:18:34 +0200

    added border around face

--- src/App.js
+++ src/App.js
@@ -43,7 +43,26 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl:'',
+      box:{},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace= data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box:box})
+    console.log(box)
   }
 
   onInputChange = (event) => {
@@ -52,17 +71,12 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-
-      }
-    );
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+          .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+          .catch(err => console.log(err));
   }
 
   render() { 
@@ -78,7 +92,7 @@ class App extends Component {
         onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box= {this.state.box} imageUrl={this.state.imageUrl} />
       </div>    
     );
   }
